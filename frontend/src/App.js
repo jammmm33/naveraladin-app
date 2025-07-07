84c0c125073a57d95e465495cafd29d5f61eac13
이유진 <jammmm33@gmail.com>
2025-07-07 23:05:58 +0900

    chore: update site title and revise UI icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ function App() {
   return (
     <div className="wrapper">
       <div className="logo-banner">
-        <h1>로고자리</h1> {/* 나중에 이미지로 교체 가능 */}
+        <h1>📘 리뷰북</h1> 
       </div>
 
       <div className="container">
         <div className="left-pane">
-          <h2 className="section-title">🩵서적 검색(키워드 검색도 가능해요)🩵</h2>
+          <h2 className="section-title">💙서적 검색(키워드 검색도 가능해요)💙</h2>
           <BookSearch />
         </div>
         <div className="right-pane">
